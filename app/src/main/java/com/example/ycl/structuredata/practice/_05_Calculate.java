b3f2c49604126f8cca8648b3bcc7aabd36474ebe
ycl <907740329@qq.com>
2021-12-11 13:21:01 +0800

    添加几个算法

--- app/src/main/java/com/example/ycl/structuredata/practice/_05_Calculate.java
+++ app/src/main/java/com/example/ycl/structuredata/practice/_05_Calculate.java
@@ -1,47 +1,29 @@
 package com.example.ycl.structuredata.practice;
-
-import java.util.Stack;
+import com.example.ycl.structuredata.practice.calculate.CalculateTest;
 
 /**
  * author:  yanchunlan
  * date:  2021/12/01 18:22
- * desc:  计算：1+2*3/4
+ * desc:
  */
 public class _05_Calculate {
 
   public static void main(String[] args) {
-    String str = "1+2*3/4";
-    double result = calculate(str);
-    System.out.println("result:444 " + result);
-  }
 
-  private static double calculate(String str) {
-    char[] array = str.toCharArray();
-    Stack<Character> stack = new Stack<>();
-    for (int i = 0; i < array.length; i++) {
-      char item = array[i];
-      if (item == '+' || item == '*' || item == '/') {
-        stack.push(item);
-      } else {
-        char result;
-        if (stack.size() > 0 && stack.peek() == '*') {
-          stack.pop();
-          result = (char) (((stack.pop() - '0') * (item - '0')) + '0');
-        } else if (stack.size() > 0 && stack.peek() == '/') {
-          stack.pop();
-          result = (char) (((stack.pop() - '0') / (item - '0')) + '0');
-        } else {
-          result = item;
-        }
-        if (i == array.length - 1) {
-          if (stack.peek() == '+') {
-            stack.pop();
-            result = (char) (((stack.pop() - '0') + (result - '0')) + '0');
-          }
-        }
-        stack.push(result);
-      }
-    }
-    return (stack.pop() - '0');
+    // 计算：1+2*3/4
+//    String str01 = "1+2*3/4";
+//    double result01 = CalculateTest.calculate_01(str01);
+//    System.out.println("_05_Calculate result: " + result01);
+
+    // 给定一个数组，其值先递增后递减，找出不重复的值
+//    int[] array02 = {1,1,1,2,3,4,9,10,9,5,4,3,2,2,2};
+//    int result02 = CalculateTest.calculate_02(array02);
+//    System.out.println("_05_Calculate result: " + result02);
+
+    // 合并n个有序链表
+    CalculateTest.mergeSortNode();
+
+    // 大文件数据排序
+
   }
 }
---
+++ app/src/main/java/com/example/ycl/structuredata/practice/_06_linked.java
@@ -0,0 +1,13 @@
+package com.example.ycl.structuredata.practice;
+
+/**
+ * author:  yanchunlan
+ * date:  2021/12/01 18:22
+ * desc:
+ */
+public class _06_linked {
+
+  public static void main(String[] args) {
+  }
+
+}
